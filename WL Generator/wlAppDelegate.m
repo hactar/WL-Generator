ebc3ccdfbca7e718328b6c9f1db24687844ee4a5
Hactar <patrick@subzero.eu>
2017-03-14 00:30:08 +0100

    added wit json creation

--- WL Generator/wlAppDelegate.m
+++ WL Generator/wlAppDelegate.m
@@ -168,6 +168,7 @@
 
 @property (readonly)  NSMutableArray *array;
 @property () NSMutableDictionary *stegDictionary;
+@property (retain) NSMutableSet *allLines;
 
 @end
 
@@ -178,10 +179,16 @@
     //NSString *stationid;
     NSMutableDictionary *_currentLine;
     BOOL firstLineDone;
+    NSRegularExpression *numberFinder;
+
 }
 
 - (void)parserDidBeginDocument:(CHCSVParser *)parser {
     _array = [[NSMutableArray alloc] init];
+    self.allLines = [[NSMutableSet alloc] init];
+    if (!numberFinder) {
+        numberFinder = [[NSRegularExpression alloc] initWithPattern:@"[0-9]+" options:0 error:nil];
+    }
 }
 - (void)parser:(CHCSVParser *)parser didBeginLine:(NSUInteger)recordNumber {
     if (firstLineDone) {
@@ -212,6 +219,34 @@
             break;
     }
 }
+
+- (NSString *)prependForSort:(NSString *)a
+{
+    
+    // undetermined order for 11A and 11B
+    NSString *preAppend= @"";
+    NSTextCheckingResult *temp = [numberFinder firstMatchInString:a options:0 range:NSMakeRange(0, [a length])];
+    NSInteger extractedNumber = 0;
+    if (temp) {
+        extractedNumber = [[a substringWithRange:temp.range] integerValue];
+    }
+    if ([a rangeOfString:@"U"].length > 0) {
+        preAppend = @"A"; //UBAHN
+    } else if ([a rangeOfString:@"N"].length > 0) {
+        preAppend = @"F"; //night line
+    } else if (([a rangeOfString:@"A"].length < 1 && [a rangeOfString:@"B"].length < 1) && a.length < 3) {
+        preAppend = @"C"; //BIM
+    } else if ([a rangeOfString:@"WLB"].length > 0) {
+        preAppend = @"D"; //BADNERBAHN
+    } else if ([a rangeOfString:@"A"].length > 0 || [a rangeOfString:@"B"].length > 0) {
+        preAppend = @"E"; //BUS
+    } else {
+        preAppend = @"G";
+    }
+    return [NSString stringWithFormat:@"%@%05ld", preAppend, (long)extractedNumber];
+}
+
+
 - (void)parser:(CHCSVParser *)parser didEndLine:(NSUInteger)recordNumber {
     if (firstLineDone) {
         if ([(NSString *)[_currentLine valueForKey:@"stationID"] length] == 0) {
@@ -223,21 +258,17 @@
             NSLog(@"No platforms for stationID %@", _currentLine);
             return;
         }
-        NSMutableString *relatedLines = [NSMutableString stringWithString:@""];
         NSMutableSet *relatedLinesSet = [NSMutableSet setWithCapacity:2];
         for (NSMutableDictionary *element in platforms) {
             NSArray *array = [[element valueForKey:@"line"] componentsSeparatedByString:@"|"];
             [relatedLinesSet addObjectsFromArray:array];
+            [self.allLines addObjectsFromArray:array];
         }
-        int i = 0;
-        for (NSString *line in relatedLinesSet) {
-            if (i > 0) {
-                [relatedLines appendFormat:@"|%@",line];
-            } else {
-                [relatedLines appendString:line];
-            }
-            i++;
-        }
+        
+        NSArray *orderedLines = [[relatedLinesSet allObjects] sortedArrayUsingComparator:^NSComparisonResult(NSString *obj1, NSString *obj2) {
+            return [[self prependForSort:obj1] compare:[self prependForSort:obj2]];
+        }];
+        NSString *relatedLines = [orderedLines componentsJoinedByString:@"|"];
         [_currentLine setValue:relatedLines forKey:@"relatedLines"];
         [_currentLine setValue:platforms forKey:@"platforms"];
         [_array addObject:_currentLine];
@@ -262,7 +293,53 @@
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
     // Insert code here to initialize your application
+    
+
 }
+
+- (NSString *) cleanName: (NSString *) name {
+    if ([name containsString:@", "]) {
+        name = [name componentsSeparatedByString:@", "][1];
+    }
+    return [[[name stringByReplacingOccurrencesOfString:@"str." withString:@"strasse"] stringByReplacingOccurrencesOfString:@"Str." withString:@"Strasse"] stringByReplacingOccurrencesOfString:@"g." withString:@"gasse"];
+}
+- (NSDictionary *) generateStationsWitDictionaryFrom: (NSArray *) arrayOfStations {
+    
+    NSMutableArray *values = [NSMutableArray array];
+    // we need to split stations and fix names first
+    
+    NSMutableSet *actualStations = [NSMutableSet set];
+    
+    [arrayOfStations enumerateObjectsUsingBlock:^(NSDictionary *station, NSUInteger idx, BOOL *stop) {
+        NSArray *arrayOfStations = [station[@"name"] componentsSeparatedByString:@"/"];
+        for (NSString *station in arrayOfStations) {
+            [actualStations addObject:[self cleanName: station]];
+        }
+    }];
+    
+    [actualStations enumerateObjectsUsingBlock:^(NSString *obj, BOOL *stop) {
+        [values addObject:@{@"value":obj, @"expressions":@[obj]}];
+
+    }];/*
+    [actualStations enumerateObjectsUsingBlock:^(NSDictionary *station, NSUInteger idx, BOOL *stop) {
+        [values addObject:@{@"value":station[@"name"], @"expressions":@[station[@"name"]]}];
+    }];
+    */
+    return @{@"values": values};
+}
+
+- (NSDictionary *) generateLinesWitDictionaryFrom: (NSArray *) arrayOfLines {
+    
+    NSMutableArray *values = [NSMutableArray array];
+    [arrayOfLines enumerateObjectsUsingBlock:^(NSString *line, NSUInteger idx, BOOL *stop) {
+        [values addObject:@{@"value":line, @"expressions":@[line]}];
+    }];
+    
+    return @{@"values": values};
+}
+
+
+
 - (IBAction)buttonPushed:(id)sender {
     
     //open linien file and generate dict
@@ -340,6 +417,21 @@
                                                                  error:nil];
                 [data writeToFile:[@"~/Desktop/wl.json" stringByExpandingTildeInPath] atomically:YES];
                 self.textView.string = @"Done. Check your desktop for wl.json";
+                
+                NSDictionary *testDict = [self generateLinesWitDictionaryFrom:[stationDelegate.allLines allObjects]];
+                //NSLog(@"all lines: %@", testDict);
+                
+                NSData *dataWitLines = [NSJSONSerialization dataWithJSONObject:testDict
+                                                                       options:kNilOptions
+                                                                         error:nil];
+                [dataWitLines writeToFile:[@"~/Desktop/witWLLines.json" stringByExpandingTildeInPath] atomically:YES];
+                
+                NSDictionary *stationsWitDict = [self generateStationsWitDictionaryFrom:stationDelegate.array];
+                
+                NSData *dataWitStations = [NSJSONSerialization dataWithJSONObject:stationsWitDict
+                                                                       options:kNilOptions
+                                                                         error:nil];
+                [dataWitStations writeToFile:[@"~/Desktop/witWLStations.json" stringByExpandingTildeInPath] atomically:YES];
                 
             }
             
